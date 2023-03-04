d3b9262f861bb0b8884aa19acb795ee565ba95f1
jharris711 <49429928+jharris711@users.noreply.github.com>
2023-03-04 18:57:43 -0500

    types

--- components/D3Demos/HorizonChart.tsx
+++ components/D3Demos/HorizonChart.tsx
@@ -14,7 +14,7 @@ interface Props {
 const x = (d: HorizonChartData) => new Date(d.date); // given d for data, returns the (temporal) x-value
 const y = (d: HorizonChartData) => d.value; // given d for data, returns the (quantitative) y-value
 const z = (d: HorizonChartData) => d.name; // given d for data, returns the (categorical) z-value
-const curve = d3.curveLinear; // method of interpolation between points
+const curve: d3.CurveGenerator = d3.curveLinear; // method of interpolation between points
 const marginTop = 20; // top margin, in pixels
 const marginRight = 0; // right margin, in pixels
 const marginBottom = 0; // bottom margin, in pixels
@@ -22,12 +22,11 @@ const marginLeft = 0; // left margin, in pixels
 const width = 640; // outer width, in pixels
 const size = 25; // outer height of a single horizon, in pixels
 const padding = 1; // separation between adjacent horizons
-const xType = d3.scaleUtc; // type of x-scale
+const xType: d3.ScaleTime<number, number, never> = d3.scaleUtc; // type of x-scale
 const xRange: number[] = [marginLeft, width - marginRight]; // [left, right]
-const yType = d3.scaleLinear; // type of y-scale
+const yType: d3.ScaleLinear<number, number, never> = d3.scaleLinear; // type of y-scale
 
 const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
-  const theme = useTheme();
   let defined: undefined | ((d: any, i: number) => boolean); // for gaps in data
   // const bands = 3; // number of bands
   let xDomain: number[]; // [xmin, xmax]
@@ -38,11 +37,15 @@ const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
   const colors = scheme[Math.max(3, bands)]; // an array of colors
 
   // Compute values.
-  const X = d3.map(data, x);
-  const Y = d3.map(data, y);
-  const Z = d3.map(data, z);
+  const X: d3.Map<string> = d3.map(data, x);
+  console.log('X', X);
+  const Y: d3.Map<number> = d3.map(data, y);
+  console.log('Y', Y);
+  const Z: d3.Map<string> = d3.map(data, z);
+  console.log('Z', Z);
   if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
-  const D = d3.map(data, defined);
+  const D: d3.Map<boolean> = d3.map(data, defined);
+  console.log('D', D);
 
   // Compute default domains, and unique the z-domain.
   if (xDomain === undefined) xDomain = d3.extent(X);
@@ -59,8 +62,8 @@ const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
   const height: number = zDomain.size * size + marginTop + marginBottom;
 
   // Construct scales and axes.
-  const xScale = xType(xDomain, xRange);
-  const yScale = yType(yDomain, yRange);
+  const xScale: d3.ScaleUtc = xType(xDomain, xRange);
+  const yScale: d3.ScaleLinear = yType(yDomain, yRange);
   const xAxis = d3
     .axisTop(xScale)
     .ticks(width / 80)
@@ -112,14 +115,14 @@ const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
       .join('g')
       .attr(
         'transform',
-        (_, i: number) => `translate(0,${i * size + marginTop})`
+        (_: any, i: number) => `translate(0,${i * size + marginTop})`
       );
 
     const defs = svg.append('defs');
 
     defs
       .append('clipPath')
-      .attr('id', (_, i: number) => `${uid}-clip-${i}`)
+      .attr('id', (_: any, i: number) => `${uid}-clip-${i}`)
       .append('rect')
       .attr('y', padding)
       .attr('width', width)
@@ -127,7 +130,7 @@ const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
 
     defs
       .append('path')
-      .attr('id', (_, i: number) => `${uid}-path-${i}`)
+      .attr('id', (_: any, i: number) => `${uid}-path-${i}`)
       .attr('d', ([, I]) => area(I));
 
     svg
@@ -136,7 +139,7 @@ const HorizonChart: React.FC<Props> = ({ bands, colorScheme, data }) => {
         return `url(${new URL(`#${uid}-clip-${i}`, l)})`;
       })
       .selectAll('use')
-      .data((d, i: number) => new Array(bands).fill(i))
+      .data((d: any, i: number) => new Array(bands).fill(i))
       .join('use')
       .attr('fill', (_, i: number) => colors[i + Math.max(0, 3 - bands)])
       .attr('transform', (_, i: number) => `translate(0,${i * size})`)
